9a5d60abfc9fb8f90b96c329e13af889ed7777a4
Kurt Catti-Schmidt (SCHMIDT) <kschmi@microsoft.com>
2026-01-27 17:08:52 -0800

    Adding files

---
+++ cssModuleElement.js
@@ -0,0 +1,14 @@
+
+import sheet from "./styles.css" assert { type: "css" };
+
+class CSSModuleTestElement extends HTMLElement {
+    constructor() {
+        super();
+        this.shadow = this.attachShadow({mode: "closed"});
+        this.shadow.adoptedStyleSheets = [sheet];
+
+        this.shadow.innerHTML = 
+            `<div class="outer-container text-container">This text should be styled</div>`;
+    }
+}
+export {CSSModuleTestElement};
